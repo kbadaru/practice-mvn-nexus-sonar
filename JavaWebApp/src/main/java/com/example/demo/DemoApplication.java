3d5ab6fa667017b4f97fc1d3cd90bcb6d76761a0
kbadaru <kbadaru@gmail.com>
2022-10-25 19:12:46 -0600

    fix

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -9,9 +9,9 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
-		String name = "kash"
+		String name = "kash";
 
-		system.out.printin(name);
+		System.out.println(name);
 	}
 
 }
